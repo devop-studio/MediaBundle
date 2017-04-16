57fab76ff11f6d3a3cbebe7f93f2d0118b73631f
Zlatin Hristov <zlatko.2create@gmail.com>
2017-04-16 21:57:44 +0300

    Update styleci configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,17 +15,17 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
                 ->children()
-                # db_driver
+                // db_driver
                 ->scalarNode('db_driver')
-                ->validate()->ifNotInArray(['orm'])->thenInvalid('db_driver must be set to one of "orm".')->end()
+                ->validate()->ifNotInArray(array('orm'))->thenInvalid('db_driver must be set to one of "orm".')->end()
                 ->defaultValue('orm')
                 ->cannotBeOverwritten()
                 ->end()
-                # upload_path
+                // upload_path
                 ->scalarNode('upload_path')
                 ->defaultValue('uploads')
                 ->end()
-                # media_formats
+                // media_formats
                 ->arrayNode('media_formats')
                 ->useAttributeAsKey('id')
                 ->prototype('array')
