2ec08873ffb73ad1a206fabda0d54582e140f8f5
Zlatin Hristov <zlatko.2create@gmail.com>
2017-04-23 11:53:14 +0300

    Little fix media collection template

--- src/Resources/public/js/media.collection.js
+++ src/Resources/public/js/media.collection.js
@@ -6,7 +6,7 @@
         e.preventDefault();
         var $collection = $($(this).data('target'));
         var $template = $($collection.data('prototype').replace(/__name__/g, $collection.find('> .form-group').length));
-        $template.find('.file').fileinput()
+        $template.find('.file').fileinput();
         $template.find('label').remove();
         $template.find('.col-sm-10').removeClass('col-sm-10').addClass('col-sm-12');
         $template.appendTo($collection);
