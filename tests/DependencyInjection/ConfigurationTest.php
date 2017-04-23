dc4458acc11cfb2b46eca9e670141369e08ecb28
Zlatin Hristov <zlatko.2create@gmail.com>
2017-04-23 16:38:49 +0300

    Adding breaking changes phpunit tests

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -2,6 +2,10 @@
 
 namespace MediaBundle\Tests;
 
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 class ConfigurationTest extends \PHPUnit_Framework_TestCase
 {
 
